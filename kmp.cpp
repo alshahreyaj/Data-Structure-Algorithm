a5cae58405dd669ac33a17c3d513d0e0368da529
Al Shahreyaj <49917196+alshahreyaj@users.noreply.github.com>
2022-04-01 16:46:41 +0600

    Create kmp.cpp

---
+++ kmp.cpp
@@ -0,0 +1,115 @@
+///----------------------///
+///---Code Written by----///
+///-----Al-Shahreyaj-----///
+///----------------------///
+ 
+///PRAGMA
+//#pragma comment(linker, "/stack:200000000")
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+//#pragma GCC optimize("unroll-loops")
+ 
+#include<bits/stdc++.h>
+ 
+///PBDS
+//#include<ext/pb_ds/assoc_container.hpp>
+//#include<ext/pb_ds/tree_policy.hpp>
+//using namespace __gnu_pbds;
+using namespace std;
+ 
+//PBDS_SET
+//Index of Value - .order_of_key()
+//Value at Index - .find_by_order()
+//typedef tree<int, null_type, less<int>, rb_tree_tag,tree_order_statistics_node_update> pbds_set; 
+//typedef tree<int, null_type, lessorequal<int>, rb_tree_tag,tree_order_statistics_node_update> pbds_multiset; 
+ 
+#define mem(a, x) memset(a, x, sizeof(a))
+#define MP make_pair
+#define inf 1e8
+#define INF 1e18
+#define CP(x) printf("Case %d: ", x);
+#define unq(v) v.erase(unique(all(v)),v.end())
+#define one(x) __builtin_popcount(x)
+#define onel(x) __builtin_popcountll(x)
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define LMX LLONG_MAX
+#define fixpre(x) cout << fixed << setprecision(x)
+#define endl "\n"
+#define vi vector<int>
+#define vll vector<long long>
+#define pii pair<int,int>
+#define pil pair<int,ll>
+#define pll pair<ll,ll>
+#define matrix vector<vector<int> >
+#define matrixll vector<vector<ll> >
+#define F first
+#define S second
+#define pb push_back
+#define pq priority_queue
+#define all(x) x.begin(),x.end()
+#define rev(x) reverse(all(x))
+#define sz(x) (int)x.size()
+#define INT ini()
+#define LONG inl()
+#define M 1000000007
+#define N 100005
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(ld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+ 
+int ini(){int x;scanf("%d",&x);return x;}
+ll inl(){ll x;scanf("%lld",&x);return x;}
+ 
+void file_mamager()
+{
+	#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+	#endif
+}
+ 
+int pr[109];
+ 
+void kmp(string s)
+{
+ 	int n = s.size();
+ 	for(int i = 1; i < n; i++)
+ 	{
+ 		int j = pr[i - 1];
+ 		while(j && s[i] != s[j]) j = pr[j - 1];
+ 		if(s[j] == s[i]) j++;
+ 		pr[i] = j;
+ 	}
+}
+ 
+int main()
+{
+	string s;
+	cin >> s;
+	kmp(s);
+	for(int i = 0; i < sz(s); i++) cout << pr[i] << " ";
+}
